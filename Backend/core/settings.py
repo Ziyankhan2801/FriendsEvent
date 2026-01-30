12fb2c14aef343987b0f2c449e2b3fca954d705e
Ziyankhan2801 <ziyankhan1363@gmail.com>
2026-01-30 16:31:03 +0530

    ABC

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -101,13 +101,25 @@ TEMPLATES = [
 # ======================
 # DATABASE
 # ======================
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # ======================
